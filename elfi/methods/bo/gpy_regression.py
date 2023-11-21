16b90eddbdf7e75ff3635ba77daf3c689fd24508
Jarrod Moss <jarrod.moss@psllab.org>
2023-11-20 19:30:38 -0600

    also apply noise prior in update method

--- elfi/methods/bo/gpy_regression.py
+++ elfi/methods/bo/gpy_regression.py
@@ -486,6 +486,7 @@ class GPyRegression:
                 kernel = self._gp.kern.copy() if self._gp.kern else None
                 noise_var = self._gp.Gaussian_noise.variance[0]
                 mean_function = self._gp.mean_function.copy() if self._gp.mean_function else None
+                noise_prior = self.gp_params.get('noise_prior')
                 if self.normalize:
                         y_mean = self.virtY.mean(axis=0)
                         y_std = self.virtY.std(axis=0)
@@ -499,6 +500,8 @@ class GPyRegression:
                 else:
                     self._gp = self._make_gpy_instance(
                             self.virtX, self.virtY, kernel=kernel, noise_var=noise_var, mean_function=mean_function)
+                if noise_prior is not None:
+                    self._gp.Gaussian_noise.set_prior(noise_prior)
                 self.optimize()
 
     def update_virt(self):
