07909cb593f92ff3eebf9806ab41a0901cd4bb92
Jarrod Moss <jarrod.moss@msstate.edu>
2024-01-13 10:39:46 -0600

    workaround pickling bug

--- elfi/methods/bo/acquisition.py
+++ elfi/methods/bo/acquisition.py
@@ -683,13 +683,13 @@ class ExpIntVar(MaxVar):
             grid_param = [slice(b[0], b[1], d_grid) for b in self.model.bounds]
             self.points_int = np.mgrid[grid_param].reshape(len(self.model.bounds), -1).T
 
-    def __setstate__(self, state):
-        del self.state['_K']
-        del self.state['K']
-        print('here')
-        self.__dict__.update(state)
-        self._K = None
-        self.K = None
+    # def __setstate__(self, state):
+    #     del self.state['_K']
+    #     del self.state['K']
+    #     print('here')
+    #     self.__dict__.update(state)
+    #     self._K = None
+    #     self.K = None
 
     def acquire(self, n, t):
         """Acquire a batch of acquisition points.
--- elfi/methods/bo/gpy_regression.py
+++ elfi/methods/bo/gpy_regression.py
@@ -129,7 +129,7 @@ class GPyRegression:
         return state
     
     def __setstate__(self, state):
-        # del state['_gp']
+        del state['_gp']
         self.__dict__.update(state)
         if self.virtual_deriv:
             # init gp
